b153a3ef3413e0f48183a7891b431f9228aed061
Ant2357 <ryu4979@gmail.com>
2024-03-21 22:41:09 +0900

    fix: console.log除去

--- background.js
+++ background.js
@@ -6,12 +6,8 @@ chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
     return;
   }
 
-  // TODO: 将来的にはこの暫定的な実装を削除する
-  console.log("時間を置いてつば九郎開始");
   const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
   await sleep(500);
-
-  console.log('Run つば九郎');
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
